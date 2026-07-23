b060ea467bec7bb19fc4fdb61be2197aab4dc374
●⃝ᴅᴀ፝֟͠ʀᴋ ✿ 𝐑_𝐀_𝐉_𝐀𓂃* <rajahalder07h@gmail.com>
2026-07-23 16:25:56 +0530

    Update config.js

--- setting/config.js
+++ setting/config.js
@@ -1,47 +1,47 @@
 const fs = require('fs')
 
 global.owner = "918509616141" //owner number
-global.footer = "༒︎ ●⃝ᴅᴀ፝֟͠ʀᴋ  ༒︎" //footer section
+global.footer = "༒︎𝑺𝒉𝒂𝒅𝒐𝒘 𝑴𝑫༒︎" //footer section
 global.status = false //"self/public" section of the bot
 global.prefa = ['','!','.','#','&','/','@']
 global.owner = ['234']
 global.xprefix = '.'
 global.gambar = "https://i.postimg.cc/NMn8rzqh/image1.png"
-global.OWNER_NAME = "@shadowhacr" //
-global.DEVELOPER = ["7848300179"] //
+global.OWNER_NAME = "@Darkraja_Official" //
+global.DEVELOPER = ["6317857334"] //
 global.BOT_NAME = "☠︎︎ 𝑺𝒉𝒂𝒅𝒐𝒘 𝑴𝑫 ☠︎︎︎"
-global.bankowner = "☠︎︎ 𓆩𝑺𝒉𝒂𝒅𝒐𝒘 𝑶𝒇𝒇𝒊𝒄𝒊𝒂𝒍𓆪 ☠︎︎"
-global.creatorName = "☠︎︎ 𓆩𝑺𝒉𝒂𝒅𝒐𝒘 𝑶𝒇𝒇𝒊𝒄𝒊𝒂𝒍𓆪 ☠︎︎"
-global.ownernumber = '923271054080'  //creator number
-global.location = "Pakistan, Punjab, sangla hill"
+global.bankowner = "●⃝ᴅᴀ፝֟͠ʀᴋ ✿ 𝐑_𝐀_𝐉_𝐀𓂃*𓆪 "
+global.creatorName = "ᴅᴀ፝֟͠ʀᴋ ✿ 𝐑_𝐀_𝐉_𝐀𓂃"
+global.ownernumber = '918509616141'  //creator number
+global.location = "India, kalkata, krishnagar"
 global.prefa = ['','!','.','#','&']
 //================DO NOT CHANGE OR YOU'LL GET AN ERROR=============\
 global.gcmess = "☠️ 𖤐 𝐀𝐂𝐂𝐄𝐒𝐒 𝐃𝐄𝐍𝐈𝐄𝐃 𖤐 ☠️  𖥂 𝐓𝐡𝐢𝐬 𝐜𝐨𝐦𝐦𝐚𝐧𝐝 𝐜𝐚𝐧 𝐨𝐧𝐥𝐲 𝐛𝐞 𝐬𝐮𝐦𝐦𝐨𝐧𝐞𝐝 𝐢𝐧 𝐭𝐡𝐞 𝐝𝐞𝐩𝐭𝐡𝐬 𝐨𝐟 𝐠𝐫𝐨𝐮𝐩𝐬.  🕳️ 𝐓𝐫𝐲𝐢𝐧𝐠 𝐢𝐭 𝐞𝐥𝐬𝐞𝐰𝐡𝐞𝐫𝐞 𝐰𝐢𝐥𝐥 𝐛𝐞 𝐢𝐠𝐧𝐨𝐫𝐞𝐝… 𝐨𝐫 𝐰𝐨𝐫𝐬𝐞."
 global.adminmess = "☠️ 𖤐 𝐀𝐃𝐌𝐈𝐍 𝐑𝐄𝐒𝐓𝐑𝐈𝐂𝐓𝐈𝐎𝐍 𖤐 ☠️  𖥂 𝐓𝐡𝐢𝐬 𝐟𝐞𝐚𝐭𝐮𝐫𝐞 𝐢𝐬 𝐫𝐞𝐬𝐭𝐫𝐢𝐜𝐭𝐞𝐝 𝐭𝐨 𝐚𝐝𝐦𝐢𝐧𝐬 𝐨𝐧𝐥𝐲.  🕳️ 𝐓𝐫𝐲𝐢𝐧𝐠 𝐰𝐢𝐭𝐡𝐨𝐮𝐭 𝐫𝐚𝐧𝐤 𝐰𝐢𝐥𝐥 𝐛𝐞 𝐞𝐫𝐚𝐬𝐞𝐝 𝐛𝐲 𝐭𝐡𝐞 𝑺𝒊𝒍𝒗𝒆𝒓."
 global.premmess = "☠️ 𖤐 𝐑𝐄𝐒𝐓𝐑𝐈𝐂𝐓𝐄𝐃 𝐅𝐄𝐀𝐓𝐔𝐑𝐄 𖤐 ☠️  𖥂 𝐓𝐡𝐢𝐬 𝐟𝐞𝐚𝐭𝐮𝐫𝐞 𝐢𝐬 𝐫𝐞𝐬𝐞𝐫𝐯𝐞𝐝 𝐟𝐨𝐫 𝐩𝐫𝐞𝐦𝐢𝐮𝐦 𝐮𝐬𝐞𝐫𝐬 𝐨𝐧𝐥𝐲.  🕳️ 𝐔𝐩𝐠𝐫𝐚𝐝𝐞 𝐲𝐨𝐮𝐫 𝐫𝐚𝐧𝐤… 𝐨𝐫 𝐟𝐚𝐥𝐥 𝐢𝐧𝐭𝐨 𝐨𝐛𝐥𝐢𝐯𝐢𝐨𝐧."
 global.dev = "918509616141"
-global.creatorName = "☠︎︎ 𓆩𝑺𝒉𝒂𝒅𝒐𝒘 𝑶𝒇𝒇𝒊𝒄𝒊𝒂𝒍𓆪 ☠︎︎'"
+global.creatorName = "ᴅᴀ፝֟͠ʀᴋ ✿ 𝐑_𝐀_𝐉_𝐀𓂃'"
 global.footer = "☠︎︎ 𝑺𝒉𝒂𝒅𝒐𝒘 𝑴𝑫 ☠︎︎" //footer section
-global.link = "https://whatsapp.com/channel/0029Vb6iopUDzgTJuzPCk32V"
+global.link = "https://whatsapp.com/channel/0029VbCtjrNEQIahN9FedJ2j"
 global.autobio = true//auto update bio
 global.botName = "☠︎︎ 𝑺𝒉𝒂𝒅𝒐𝒘 𝑴𝑫 ☠︎︎"
 global.version = "V2"
 global.botname = "☠︎︎ 𝑺𝒉𝒂𝒅𝒐𝒘 𝑴𝑫 ☠︎︎"
-global.author = "༒︎ 𝑺𝑯𝑨𝑫𝑶𝑾 ༒︎"
+global.author = "ᴅᴀ፝֟͠ʀᴋ ✿ 𝐑_𝐀_𝐉_𝐀𓂃"
 global.themeemoji = '🤞'
-global.wagc = 'https://whatsapp.com/channel/0029Vb6iopUDzgTJuzPCk32V'
+global.wagc = 'https://whatsapp.com/channel/0029VbCtjrNEQIahN9FedJ2j'
 global.thumbnail = 'https://i.postimg.cc/qvrFRzxG/thumb.png'
 global.richpp = 'https://i.postimg.cc/jjdkHm9n/scar1.png'
 global.packname = "𝐒𝐓𝐈𝐂𝐊𝐄𝐑𝐒 𖣘♔︎"
-global.author = "༒︎ 𝑺𝑯𝑨𝑫𝑶𝑾 ༒︎"
+global.author = "ᴅᴀ፝֟͠ʀᴋ ✿ 𝐑_𝐀_𝐉_𝐀𓂃"
 global.creator = "918509616141@s.whatsapp.net"
-global.ownername = '☠︎︎ 𝑺𝒉𝒂𝒅𝒐𝒘 𝑶𝒇𝒇𝒊𝒄𝒊𝒂𝒍 ☠︎︎' 
+global.ownername = 'ᴅᴀ፝֟͠ʀᴋ ✿ 𝐑_𝐀_𝐉_𝐀𓂃' 
 global.onlyowner = `☠️ 𝙎𝙤𝙧𝙧𝙮, 𝙏𝙝𝙞𝙨 𝙁𝙚𝙖𝙩𝙪𝙧𝙚 𝙄𝙨 𝙍𝙚𝙨𝙩𝙧𝙞𝙘𝙩𝙚𝙙 𝙏𝙤 𝙊𝙬𝙣𝙚𝙧𝙨 𝙊𝙣𝙡𝙮.  
-🩸 𝘾𝙤𝙣𝙩𝙖𝙘𝙩 *SHADOW* 𝙩𝙤 𝙜𝙖𝙞𝙣 𝙤𝙬𝙣𝙚𝙧 𝙥𝙧𝙞𝙫𝙞𝙡𝙚𝙜𝙚𝙨.  
+🩸 𝘾𝙤𝙣𝙩𝙖𝙘𝙩 *ᴅᴀ፝֟͠ʀᴋ ✿ 𝐑_𝐀_𝐉_𝐀𓂃* 𝙩𝙤 𝙜𝙖𝙞𝙣 𝙤𝙬𝙣𝙚𝙧 𝙥𝙧𝙞𝙫𝙞𝙡𝙚𝙜𝙚𝙨.  
 🕳️ 𝙄𝙡𝙡𝙚𝙜𝙖𝙡 𝙖𝙘𝙘𝙚𝙨𝙨 𝙬𝙞𝙡𝙡 𝙗𝙚 𝙘𝙡𝙚𝙖𝙣𝙨𝙚𝙙 𝙗𝙮 𝙩𝙝𝙚 𝑻𝒂𝒍𝒉𝒂.`
   // reply 
 global.database = `☠️ 𝙏𝙤 𝙗𝙚 𝙖𝙙𝙙𝙚𝙙 𝙩𝙤 𝙩𝙝𝙚 𝘿𝘼𝙏𝘼𝘽𝘼𝙎𝙀 𝘽𝘼𝙎𝙀...  
-🩸 𝘾𝙤𝙣𝙩𝙖𝙘𝙩 *SHADOW* — 𝙊𝙧 𝙗𝙚 𝙚𝙧𝙖𝙨𝙚𝙙 𝙗𝙮 𝙙𝙚𝙛𝙖𝙪𝙡𝙩.  🔗 *No entry without approval.*`
+🩸 𝘾𝙤𝙣𝙩𝙖𝙘𝙩 *ᴅᴀ፝֟͠ʀᴋ ✿ 𝐑_𝐀_𝐉_𝐀𓂃* — 𝙊𝙧 𝙗𝙚 𝙚𝙧𝙖𝙨𝙚𝙙 𝙗𝙮 𝙙𝙚𝙛𝙖𝙪𝙡𝙩.  🔗 *No entry without approval.*`
   global.mess = {
 wait: "```☠️ WAIT FOR  ☠︎︎ 𝑺𝒉𝒂𝒅𝒐𝒘 𝑴𝑫 ☠︎︎😈 ⚡ *Darkness intensifies...*  💀 *Brace yourself — The SHADOW is rising.*```",
    success: "✔️ 𝗦𝗨𝗖𝗖𝗘𝗦𝗦 𝗕𝗬 ☠︎︎ 𝑺𝒉𝒂𝒅𝒐𝒘 𝑴𝑫 ☠︎︎ ⚔️`  🔥 *Mission complete. The Void accepts your command.*",
@@ -58,33 +58,33 @@ wait: "```☠️ WAIT FOR  ☠︎︎ 𝑺𝒉𝒂𝒅𝒐𝒘 𝑴𝑫 ☠︎︎
    only: {
        group: "Sorry bro, This Feature Can Only Be Used and locked to  Group chat  only",
 private: "Sorry bro, This Feature Can Only Be Used In Private Chats",
-       owner: "Sorry bro, This Feature Can Only Be Used by ☠︎︎ 𝑺𝒉𝒂𝒅𝒐𝒘 𝑶𝒇𝒇𝒊𝒄𝒊𝒂𝒍 ☠︎︎brr😋",
+       owner: "Sorry bro, This Feature Can Only Be Used by ᴅᴀ፝֟͠ʀᴋ ✿ 𝐑_𝐀_𝐉_𝐀𓂃",
        admin: " Sorry, this feature can only be used by Bot Admins",
        badmin: "Sorry, bro, It Looks Like You Can't Use This Feature Because the Bot is Not yet Group Admin",
-       premium: "This feature is specifically for my owner ༒︎ 𝑺𝑯𝑨𝑫𝑶𝑾 ༒︎🤤🫡",
+       premium: "This feature is specifically for my owner ᴅᴀ፝֟͠ʀᴋ ✿ 𝐑_𝐀_𝐉_𝐀𓂃",
    }
 }
 
-global.onlyowner = `\`[ 🤞 ] ☠︎︎ 𝑺𝒉𝒂𝒅𝒐𝒘 𝑴𝑫 ☠︎︎ \` \n*
+global.onlyowner = `\`[ 🤞 ] ᴅᴀ፝֟͠ʀᴋ ✿ 𝐑_𝐀_𝐉_𝐀𓂃\` \n*
 
          🚫 𝔸ℂℂ𝔼𝕊𝕊 𝔻𝔼ℕ𝕀𝔼𝔻 🚫
 
 𝕊𝕆ℝℝ𝕐... 𝕋ℍ𝕀𝕊 𝕔𝕠𝕞𝕞𝕒𝕟𝕕 𝕓𝕝𝕠𝕠𝕕𝕤𝕙𝕖𝕕𝕤 𝕗𝕠𝕣 𝕐𝕆𝕌
 𝕆ℕ𝕃𝕐 𝔹𝕃𝔸𝔻𝔼𝔻 𝕌𝕊𝔼ℝ𝕊 𝕀ℕ 𝕆𝕌ℝ 𝔻𝔸𝕋𝔸𝔹𝔸𝕊𝔼 𝕄𝔸𝕐 𝔼𝕟𝕥𝕖𝕣 ⚔️ `
   
-global.database = `\`[ 👑 ] ☠︎︎ 𝑺𝒉𝒂𝒅𝒐𝒘 𝑴𝑫 ☠︎︎ 😙 \` \n*
+global.database = `\`[ 👑 ] ᴅᴀ፝֟͠ʀᴋ ✿ 𝐑_𝐀_𝐉_𝐀𓂃\` \n*
 
         🚫 𝔸ℂℂ𝔼𝕊𝕊 𝔻𝔼ℕ𝕀𝔼𝔻 🚫
 
 ❌ *𝗦𝗢𝗥𝗥𝗬, 𝗧𝗛𝗜𝗦 𝗖𝗢𝗠𝗠𝗔𝗡𝗗 𝗜𝗦 𝗙𝗢𝗥𝗕𝗜𝗗𝗗𝗘𝗡 𝗙𝗢𝗥 𝗬𝗢𝗨!* ❌
 
-💀 *𝗢𝗡𝗟𝗬 𝗦𝗛𝗔𝗗𝗢𝗪 𝗠𝗘𝗠𝗕𝗘𝗥𝗦 𝗜𝗡 𝗢𝗨𝗥 𝗗𝗔𝗧𝗔𝗕𝗔𝗦𝗘 𝗛𝗢𝗟𝗗 𝗧𝗛𝗘 𝗞𝗘𝗬 𝗧𝗢 𝗔𝗖𝗖𝗘𝗦𝗦.* 😈
+💀 *𝗢𝗡𝗟𝗬 Dark 𝗠𝗘𝗠𝗕𝗘𝗥𝗦 𝗜𝗡 𝗢𝗨𝗥 𝗗𝗔𝗧𝗔𝗕𝗔𝗦𝗘 𝗛𝗢𝗟𝗗 𝗧𝗛𝗘 𝗞𝗘𝗬 𝗧𝗢 𝗔𝗖𝗖𝗘𝗦𝗦.* 😈
 
 𝕮𝖔𝖓𝖙𝖆𝖈𝖙 𝕺𝖚𝖗 𝕺𝖋𝖋𝖎𝖈𝖎𝖆𝖑𝖘 𝕭𝖊𝖋𝖔𝖗𝖊 𝖞𝖔𝖚 𝖋𝖆𝖑𝖑:
 
-🔥 𝖂𝖆: wa.me/923271054080
-🕷️ 𝖙𝖌: t.me/shadowhacr
-💀 𝖜𝖆 𝖈𝖍𝖆𝖓𝖓𝖊𝖑: https://whatsapp.com/channel/0029Vb6iopUDzgTJuzPCk32V
+🔥 𝖂𝖆: wa.me/918509616141
+🕷️ 𝖙𝖌: t.me/Darkraja_Official
+💀 𝖜𝖆 𝖈𝖍𝖆𝖓𝖓𝖊𝖑: https://whatsapp.com/channel/0029VbCtjrNEQIahN9FedJ2j
 
 𝕋𝕙𝕒𝕟𝕜𝕤 𝕗𝕠𝕣 𝕌𝕤𝕚𝕟𝕘  ☠︎︎ 𝑺𝒉𝒂𝒅𝒐𝒘 𝑴𝑫 ☠︎︎`
 
